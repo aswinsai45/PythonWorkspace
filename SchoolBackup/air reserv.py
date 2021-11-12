e942eedecf5cd41239a8727d6da66b6752692e7a
aswinsai45 <67233791+aswinsai45@users.noreply.github.com>
2021-11-12 17:17:54 -0500

    School project finished

--- SchoolBackup/air reserv.py
+++ SchoolBackup/air reserv.py
@@ -1,122 +1,41 @@
-import datetime
-import random
+import os.path
+import shutil
 
-titrd = datetime.date.today()
+print("Welcome to FlightBook")
 
-print("Welcome to Aires Booking")
-ch = int(input("Enter the number of passengers travelling "))
+action = input('What would you like to do?\n 1. Book a ticket\n 2. Retrieve a ticket\n 3. Cancel a ticket?\n '
+               'Available commands are "book", "cancel", "retrieve": ')
 
-if ch == 1:
-    gen = random.randint(2000, 9000)
-    print('Enter your details')
-    P0 = input("enter your name: ")
-    DOB = (input("enter your Date of Birth in dd/mm/yyyy format: "))
-    dobd = datetime.datetime.strptime(DOB, "%d/%m/%Y").date()
-    if dobd > titrd.today():
-        print("DOB cannot be selected in the future, you have chose ", DOB)
-        exit()
-    else:
-        pass
-    TrDate = (input("enter departure date in dd/mm/yyyy format "))
-    trdated = datetime.datetime.strptime(TrDate, "%d/%m/%Y").date()
-    if trdated >= titrd.today():
+if action == str('retrieve') and str('Retrieve'):
+    bpass = input('Enter boarding pass number here: ')
+    file_exist = os.path.exists('C:/Users/aswin/PycharmProjects/SchoolProject/BookingConf/' + bpass)
+    if file_exist:
         pass
     else:
-        print("Cannot choose a date in the past", TrDate)
+        print('Booking Cancelled/Not Found')
         exit()
 
-    pno = input("Enter your Passport Number: ")
-    pnex = (input("Enter expiration date of passport in dd/mm/yyyy format: "))
-    pnext = datetime.datetime.strptime(pnex, "%d/%m/%Y").date()
-    if pnext >= titrd.today():
-        pass
-    else:
-        print("Renew your passport and try again")
-        exit()
-    # departure and destinations
-    print("Available Departure and Destination Locations are: " '\n' + '1. USA' + '\n' + '2. Canada' + '\n' + '3. '
-                                                                                                              'Mexico' + '\n' + '4. UK' + '\n' + '5. India' + '\n')
-    print('Departure/Arrival to limited countries due to Covid -19,\nwe suggest checking travel guidelines of the '
-          'country you will be travelling to before booking your ticket')
-    li0 = [1, 2, 3, 4, 5]
-    dep = input('Where are you departing from?')
-    if dep not in li0:
-        print('Choose a valid country number of the country you wish to travel')
-    else:
-        pass
-    dest = input('Where do you wish to travel?: ')
-    if dest not in li0:
-        print('Choose a valid country number of the country you wish to travel')
-    else:
-        pass
-    f = open("C:/Users/aswin/PycharmProjects/SchoolProject/BookingConf/" + str(gen), 'a+')
-    f.write('PASSENGER 1: ' + '\n')
-    f.write(
-        'Name: ' + P0 + '\n' + 'Date of Birth: ' + DOB + '\n' 'Passport Number: ' + pno + '\n' 'Expiry Date : ' + pnex + '\n')
-    f.write('Your Journey is on: ' + TrDate + '\n')
-    f.write('Departure: ' + dep + '|' + 'Destination: ' + dest)
-    f.write("Your Boarding Pass Number is AX" + str(gen) + '\n')
-    f.close()
-    exit()
-############# if CH2
-if ch == 2:
-    gen = random.randint(2000, 9000)
-    P1 = input("Enter Name of Passenger 1: ")
-    DOB1 = (input("enter your Date of Birth in dd/mm/yyyy format "))
-    dobd = datetime.datetime.strptime(DOB1, "%d/%m/%Y").date()
-    if dobd > titrd.today():
-        print("DOB cannot be selected, you have chose ", DOB1)
-        exit()
-    else:
-        pass
-    TrDate = (input("enter departure date in dd/mm/yy format "))
-    trdated = datetime.datetime.strptime(TrDate, "%d/%m/%Y").date()
-    if trdated >= titrd.today():
-        pass
-    else:
-        print("Cannot choose date", trdated)
+    file = open('C:/Users/aswin/PycharmProjects/SchoolProject/BookingConf/' + bpass, 'r')
+    s = file.read()
+    print(s)
+
+elif action == str('book') and str('Book'):
+    pass
+    ch = int(input("\nEnter the number of people travelling [up-to 3]: "))
+    if ch == 1:
+        import singleperson
+    elif ch == 2:
+        import twopersons
+    elif ch == 3:
+        import threepersons
+    elif ch > 3:
+        print("Sorry unavailable as of now. Thanks for understanding!")
         exit()
 
-    pno1 = input("Enter your Passport Number: ")
-    pnex1 = (input("Enter expiration date of passport in dd/mm/yyyy format: "))
-    pnext = datetime.datetime.strptime(pnex1, "%d/%m/%Y").date()
-    if pnext >= titrd.today():
-        pass
-    else:
-        print("Renew your passport and try again")
-        exit()
-    # P2 CH2
-    P2 = input("Enter Name of Passenger 2: ")
-    DOB2 = (input("enter your Date of Birth in dd/mm/yyyy format "))
-    dobd = datetime.datetime.strptime(DOB2, "%d/%m/%Y").date()
-    if dobd > titrd.today():
-        print("DOB cannot be selected, you have chose ", DOB2)
-        exit()
-    else:
-        pass
-    TrDate = (input("enter departure date in dd/mm/yy format "))
-    trdated = datetime.datetime.strptime(TrDate, "%d/%m/%Y").date()
-    if trdated >= titrd.today():
-        pass
-    else:
-        print("Cannot choose date", trdated)
-        exit()
+elif action == str('cancel') and str('Cancel'):
+    bpassc = input('Please enter boarding pass number here: ')
 
-    pno2 = input("Enter your Passport Number: ")
-    pnex2 = (input("Enter expiration date of passport in dd/mm/yyyy format: "))
-    pnext = datetime.datetime.strptime(pnex2, "%d/%m/%Y").date()
-    if pnext >= titrd.today():
-        pass
-    else:
-        print("Renew your passport and try again")
-        exit()
-    f = open("C:/Users/aswin/PycharmProjects/SchoolProject/BookingConf/" + str(gen), 'a+')
-    f.write('PASSENGER 1: ' + '\n')
-    f.write(P1 + '\n' + DOB1 + '\n' + pno1 + '\n' + pnex1 + '\n')
-    f.write('PASSENGER 2: ' + '\n')
-    f.write(
-        P2 + '\n' + 'Date of Birth: ' + DOB2 + '\n' + 'Passport Number: ' + pno2 + '\n' + 'Expiry Date: ' + pnex2 + '\n')
-    f.write('\n' + 'Travel Info: ')
-    f.write('Journey Date: ' + TrDate + '\n')
-    f.write("Your Boarding Pass Number is AX" + str(gen))
-    f.close()
+    shutil.move('C:/Users/aswin/PycharmProjects/SchoolProject/BookingConf/' + bpassc, 'C:/Users/aswin/PycharmProjects'
+                                                                                      '/SchoolProject/BookingConf'
+                                                                                      '/Cancelled')
+    print('Done! Ticket ' + bpassc + ' has been cancelled')
---
+++ SchoolBackup/threepersons.py
@@ -0,0 +1,213 @@
+import datetime
+import random
+
+titrd = datetime.date.today()
+
+gen = random.randint(2000, 9000)
+randf = random.randint(2252, 9252)
+
+#P(0)
+
+print('ENTER the DETAILS CAREFULLY')
+P0 = input("\nenter Passenger 1's name: ")
+DOB = (input("enter Passenger 1's Date of Birth in dd/mm/yyyy format: "))
+dobd = datetime.datetime.strptime(DOB, "%d/%m/%Y").date()
+
+if dobd >= titrd.today():
+    print("Cannot choose a date in the future, -", DOB, "\n\nPlease try again//\n")
+    chdob = input("Do you want to try again? [Enter Y or N]: ")
+    if chdob == 'Y' or 'y':
+        print("\nEnter the date correctly this time, or else you will be exited// ")
+        DOB = input("Enter Date of Birth in dd/mm/yyyy format: ")
+        dobd = datetime.datetime.strptime(DOB, "%d/%m/%Y").date()
+        if dobd < titrd.today():
+            pass
+        else:
+            print("Cannot choose a date in the future, -", DOB)
+            print("Sorry, please restart and try again//")
+            exit()
+    elif chdob == 'N' or 'n':
+        print('Okay, you will now be exited out of the program')
+        exit()
+
+else:
+    pass
+
+pno = input("Enter Passenger 1's Passport Number: ")
+pnex = (input("Enter expiration date of passport in dd/mm/yyyy format: "))
+pnext = datetime.datetime.strptime(pnex, "%d/%m/%Y").date()
+if pnext > titrd.today():
+    pass
+else:
+    chyn = input("\n" + "Invalid date/format. Date cannot be in the past, do you want to try again? [Y or N]")
+    if chyn == 'Y' or 'y':
+        print('\n' + "Please enter correctly this time or else you will be exited..")
+        pnex = (input("Expiry date of passport [dd/mm/yyyy]: "))
+        pnext = datetime.datetime.strptime(pnex, "%d/%m/%Y").date()
+        if pnext > titrd.today():
+            pass
+        else:
+            print('\n' + 'Sorry, date invalid, date cannot be in the past', pnex)
+            print('Please restart and try again')
+            exit()
+    if chyn == 'N' or 'n':
+        print('Okay, you will now be exited out of the program')
+        exit()
+
+#P(2)
+
+print("PASSENGER 2: ")
+
+print('ENTER THE DETAILS CAREFULLY')
+P1 = input("\nEnter Passenger 2's name: ")
+DOB1 = (input("enter Passenger 2's DOB in dd/mm/yyyy format: "))
+dobd = datetime.datetime.strptime(DOB1, "%d/%m/%Y").date()
+
+if dobd >= titrd.today():
+    print("Cannot choose a date in the future, -", DOB, "\n\nPlease try again//\n")
+    chdob = input("Do you want to try again? [Enter Y or N]: ")
+    if chdob == 'Y' or 'y':
+        print("\nEnter the date correctly this time, or else you will be exited// ")
+        DOB = input("Enter Date of Birth in dd/mm/yyyy format: ")
+        dobd = datetime.datetime.strptime(DOB1, "%d/%m/%Y").date()
+        if dobd < titrd.today():
+            pass
+        else:
+            print("Cannot choose a date in the future, -", DOB1)
+            print("Sorry, please restart and try again//")
+            exit()
+    elif chdob == 'N' or 'n':
+        print('Okay, you will now be exited out of the program')
+        exit()
+
+else:
+    pass
+
+pno1 = input("Enter Passenger 2's Passport Number: ")
+pnex1 = (input("Enter expiration date of passport in dd/mm/yyyy format: "))
+pnext = datetime.datetime.strptime(pnex1, "%d/%m/%Y").date()
+if pnext > titrd.today():
+    pass
+else:
+    chyn = input("\n" + "Invalid date/format. Date cannot be in the past, do you want to try again? [Y or N]")
+    if chyn == 'Y' or 'y':
+        print('\n' + "Please enter correctly this time or else you will be exited..")
+        pnex1 = (input("Expiry date of passport [dd/mm/yyyy]: "))
+        pnext = datetime.datetime.strptime(pnex1, "%d/%m/%Y").date()
+        if pnext > titrd.today():
+            pass
+        else:
+            print('\n' + 'Sorry, date invalid, date cannot be in the past', pnex)
+            print('Please restart and try again')
+            exit()
+    if chyn == 'N' or 'n':
+        print('Okay, you will now be exited out of the program')
+        exit()
+
+#P(3)
+
+print("PASSENGER 2: ")
+
+print('ENTER THE DETAILS CAREFULLY')
+P2 = input("\nEnter Passenger 2's name: ")
+DOB2 = (input("enter Passenger 2's DOB in dd/mm/yyyy format: "))
+dobd = datetime.datetime.strptime(DOB2, "%d/%m/%Y").date()
+
+if dobd >= titrd.today():
+    print("Cannot choose a date in the future, -", DOB, "\n\nPlease try again//\n")
+    chdob = input("Do you want to try again? [Enter Y or N]: ")
+    if chdob == 'Y' or 'y':
+        print("\nEnter the date correctly this time, or else you will be exited// ")
+        DOB = input("Enter Date of Birth in dd/mm/yyyy format: ")
+        dobd = datetime.datetime.strptime(DOB1, "%d/%m/%Y").date()
+        if dobd < titrd.today():
+            pass
+        else:
+            print("Cannot choose a date in the future, -", DOB1)
+            print("Sorry, please restart and try again//")
+            exit()
+    elif chdob == 'N' or 'n':
+        print('Okay, you will now be exited out of the program')
+        exit()
+
+else:
+    pass
+
+pno2 = input("Enter Passenger 3's Passport Number: ")
+pnex2 = (input("Enter expiration date of passport in dd/mm/yyyy format: "))
+pnext = datetime.datetime.strptime(pnex2, "%d/%m/%Y").date()
+if pnext > titrd.today():
+    pass
+else:
+    chyn = input("\n" + "Invalid date/format. Date cannot be in the past, do you want to try again? [Y or N]")
+    if chyn == 'Y' or 'y':
+        print('\n' + "Please enter correctly this time or else you will be exited..")
+        pnex1 = (input("Expiry date of passport [dd/mm/yyyy]: "))
+        pnext = datetime.datetime.strptime(pnex1, "%d/%m/%Y").date()
+        if pnext > titrd.today():
+            pass
+        else:
+            print('\n' + 'Sorry, date invalid, date cannot be in the past', pnex)
+            print('Please restart and try again')
+            exit()
+    if chyn == 'N' or 'n':
+        print('Okay, you will now be exited out of the program')
+        exit()
+
+print('\n DEPARTURE AND DESTINATION: ')
+TrDate = (input("enter departure date in dd/mm/yyyy format "))
+trdated = datetime.datetime.strptime(TrDate, "%d/%m/%Y").date()
+if trdated >= titrd.today():
+    pass
+else:
+    print("Cannot choose a date in the past, -", TrDate + '\n')
+    chtr = input("Do you want to try try again? [Enter Y or N]: ")
+    if chtr == 'Y' or 'y':
+        TrDate = (input("enter departure date in dd/mm/yyyy format: "))
+        trdated = datetime.datetime.strptime(TrDate, "%d/%m/%Y").date()
+        if trdated >= titrd.today():
+            pass
+        else:
+            print('\n' + 'Sorry, date invalid, date cannot be in the past')
+            print('Please restart and try again')
+            exit()
+    elif chtr == 'N' or 'n':
+        print('Okay, you will now be exited out of the program')
+        exit()
+
+##############################
+print(
+    '\n' + 'Departure/Arrival to limited countries due to Covid -19,\n we suggest checking with the guidelines of '
+           'the place you are travelling to\n')
+
+print("Now enter your Departure Location below")
+depco = input("Departing Country?: " + '\n')
+depci = input("Departing City?: " + '\n')
+
+print("Now enter your Arrival Location below\n")
+destco = input("Destination Country?: ")
+destci = input("Destination City?: ")
+
+print("1. Air India" + '\n' + "2. United Airlines" + '\n' + "3. Emirates" + '\n' + "4. Etihad Airways" + '\n' + "5. Qatar Airways" + '\n')
+
+
+
+import testingairline
+
+print("YOUR TICKET HAS BEEN BOOKED SUCCESSFULLY. YOUR TICKET NUMBER IS AX", str(gen))
+f = open("C:/Users/aswin/PycharmProjects/SchoolProject/BookingConf/" + 'AX' + str(gen), 'a+')
+f.write('PASSENGER 1: ' + '\n')
+f.write(
+    'Name: ' + P0 + '\n' + 'Date of Birth: ' + DOB + '\n' 'Passport Number: ' + pno + '\n' 'Expiry Date : ' + pnex + '\n')
+f.write('\nPASSENGER 2: ' + '\n')
+f.write(
+    'Name: ' + P1 + '\n' + 'Date of Birth: ' + DOB1 + '\n' 'Passport Number: ' + pno1 + '\n' 'Expiry Date : ' + pnex1 + '\n')
+#for person 3
+f.write('\nPASSENGER 3: ' + '\n')
+f.write(
+    'Name: ' + P2 + '\n' + 'Date of Birth: ' + DOB2 + '\n' 'Passport Number: ' + pno2 + '\n' 'Expiry Date : ' + pnex2 + '\n')
+f.write('Your Journey is on: ' + TrDate + '\n')
+f.write('\nDeparture: ' + depci + ', ' + depco + '  |  ' + 'Destination: ' + destci + ', ' + destco + '\n')
+f.write("\nYour Boarding Pass Number is AX" + str(gen) + '\n')
+f.close()
+exit()
