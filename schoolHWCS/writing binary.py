cef53bbcaa7d6a37aeee98a36ecac54ef45a6a15
aswinsai45 <aswinsai45@gmail.com>
2022-06-06 21:56:38 +0530

    binary

--- schoolHWCS/writing binary.py
+++ schoolHWCS/writing binary.py
@@ -1,6 +1,21 @@
 import pickle
-f = open('try.dat', 'rb')
-k = pickle.load(f)
-f.close()
-print(k)
-`
+file = open('try.dat', 'rb')
+l1 = []
+while True:
+    admn = input("Enter admission number: ")
+    name = input('Enter name: ')
+    fname = input("Enter father's name: ")
+    mname = input("Enter mother's name:")
+    add = input('Enter address: ')
+    l1.append(admn)
+    l1.append(name)
+    l1.append(fname)
+    l1.append(mname)
+    l1.append(add)
+    pickle.dump(l1, file)
+    s = input('Do you want to stop? [y/n]: ')
+    if s == 'y' or s == 'y':
+        print('Stopping..')
+        break
+s = pickle.load(file)
+print(s)
